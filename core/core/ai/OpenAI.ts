f1befc7ae5e64783c566fdb429793ec561c7aa53
Weredok <derovinj@gmail.com>
2026-07-15 18:34:39 +0300

    fix: deleted unused files + env problems

--- core/core/ai/OpenAI.ts
+++ core/core/ai/OpenAI.ts
@@ -1,11 +1,7 @@
 import OpenAI from 'openai';
-import fs from "node:fs";
-import dotenv from "dotenv";
-dotenv.config();
-
 
 const client = new OpenAI({
-  apiKey: "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", // This is the default and can be omitted
+  apiKey: process.env.openai
 });
 
 export { client as OpenAIClient}
